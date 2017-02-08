b84f3a4496acaf9f1c0640a1140d0b0fa9db63c5
erinflynn <eflynn3@nd.edu>
2017-02-07 19:29:04 -0500

    Basic list for homepage completed

--- app/src/main/java/mobilend/myapplication/MainActivity.java
+++ app/src/main/java/mobilend/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package mobilend.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    private ListView HomePageListView;
+    private ArrayAdapter<String> listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HomePageListView = (ListView) findViewById(R.id.home_page_list);
+
+        String[] listOptions = new String [] {"Life", "Work", "Business", "Government"};
+        ArrayList<String> homeList = new ArrayList<String>();
+        homeList.addAll( Arrays.asList(listOptions) );
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, homeList);
+
+        HomePageListView.setAdapter( listAdapter );
     }
 }
+
