eeb4cfc5a4fafa32b65a382aeba81d164e41cc44
erinflynn <eflynn3@nd.edu>
2017-02-08 11:57:17 -0500

    iml file?

--- app/src/main/java/mobilend/myapplication/MainActivity.java
+++ app/src/main/java/mobilend/myapplication/MainActivity.java
@@ -5,13 +5,17 @@ import java.util.Arrays;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private ListView HomePageListView;
-    private ArrayAdapter<String> listAdapter ;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,18 @@ public class MainActivity extends Activity {
 
         HomePageListView = (ListView) findViewById(R.id.home_page_list);
 
-        String[] listOptions = new String [] {"Life", "Work", "Business", "Government"};
+        String[] listOptions = new String[]{"Life", "Work", "Business", "Government"};
         ArrayList<String> homeList = new ArrayList<String>();
-        homeList.addAll( Arrays.asList(listOptions) );
+        homeList.addAll(Arrays.asList(listOptions));
         listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, homeList);
 
-        HomePageListView.setAdapter( listAdapter );
+        HomePageListView.setAdapter(listAdapter);
+
+        /*HomePageListView.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                startActivity(new Intent(MainActivity.this, NextActivity.class));
+            }
+        });*/
     }
 }
 
